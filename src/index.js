433cae960123feb8acbfe9e57267611948f42726
cassfer <user.email>
2020-09-15 17:48:47 -0300

    inicio repositorio

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
+import ReactDOM from 'react-dom'
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+import Primeiro from './components/basicos/Primeiro';
+import ComParametro from './components/basicos/ComParametro';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    <div>
+        <Primeiro />
+        <ComParametro 
+            titulo="Situação do aluno" 
+            aluno="Pedro" nota={8.0} />
+    </div>,
+    document.getElementById('root')
+)
