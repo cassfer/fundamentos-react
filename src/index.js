0623403fb5b824865b973124cf0169a0453a9ed6
cassfer <user.email>
2020-09-15 18:02:26 -0300

    finalizado react fragment

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import './index.css';
 
 import Primeiro from './components/basicos/Primeiro';
 import ComParametro from './components/basicos/ComParametro';
+import Fragmento from './components/basicos/Fragmento';
 
 
 ReactDOM.render(
-    <div>
+    <div id="app">
+        <Fragmento/>
         <Primeiro />
         <ComParametro 
             titulo="Situação do aluno" 
